b487fd623e61133f9a0a9a4f23b513b8d008c467
Tom Arnfeld <tarnfeld@cloudflare.com>
2016-08-03 23:58:35 +0100

    Write the coords to a file on the SD card

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,21 @@
 
 #include "power.h"
 #include "gps.h"
+#include "SD/sd-card-library-photon-compat.h"
+
+const uint8_t chipSelect = A2;
+const uint8_t mosiPin = A5;
+const uint8_t misoPin = A4;
+const uint8_t clockPin = A3;
 
 void setup() {
     Power::setup();
     GPS::setup();
+
+    // Write the location to the SD card
+    if (!SD.begin(mosiPin, misoPin, clockPin, chipSelect)) {
+        Particle.publish("sderr", "Card failed, or not present.");
+    }
 }
 
 void loop() {
@@ -20,7 +31,13 @@ void loop() {
     std::string pos = GPS::readPosition(1000);
     Particle.publish("gpsdata", pos.c_str());
 
-    delay(5 * 1000);
+    File dataFile = SD.open("gps-coords.txt", FILE_WRITE);
+    if (dataFile) {
+        dataFile.seek(dataFile.size()); // Move to the end of the file
+        dataFile.println(pos.c_str());
+        dataFile.flush();
+        dataFile.close();
+    }
 
-    System.sleep(SLEEP_MODE_DEEP, 30);
+    delay(5 * 1000);
 }
